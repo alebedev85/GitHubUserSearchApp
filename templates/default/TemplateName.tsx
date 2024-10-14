102257c308ac5973015ac0a1c383367a8ff9c040
Andrey Lebedev <alebedev424@gmail.com>
2024-10-14 15:11:48 +0300

    feat: создал проект, добавил типы

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./TemplateName.module.scss";
+
+interface TemplateNameProps {}
+
+export const TemplateName = ({}: TemplateNameProps) => (
+  <div className={styles.templateName}>TemplateName Component</div>
+);
