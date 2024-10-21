3344a720b1c1940d40a13285e0054e1b72ef8e5d
Andrey Lebedev <alebedev424@gmail.com>
2024-10-21 11:32:47 +0300

    feat: добавил блок заголовков

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,15 +1,21 @@
 import { UserStat } from "components/UserStat";
 import styles from "./UserCard.module.scss";
 import { LocalGithubUser } from "types";
+import { UserTitle } from "components/UserTitle";
 
-interface UserCardProps extends LocalGithubUser{}
+interface UserCardProps extends LocalGithubUser {}
 
 export const UserCard = (props: UserCardProps) => (
   <div className={styles.userCard}>
-    <UserStat 
-    repos={props.repos}
-    followers={props.followers}
-    following={props.following}
+    <img className={styles.avatar} src={props.avatar} alt={props.login} />
+    <UserTitle name={props.name} login={props.login} created={props.created} />
+    <p className={`${styles.bio}${props.bio ? '': `${styles.empty}`}`}>
+      {props.bio || 'This profile has no bio'}
+    </p>
+    <UserStat
+      repos={props.repos}
+      followers={props.followers}
+      following={props.following}
     />
   </div>
 );
