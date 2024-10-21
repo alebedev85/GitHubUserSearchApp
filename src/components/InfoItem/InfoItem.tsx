e8ee85d581047b95b068c2893c80a5cf3128308b
Andrey Lebedev <alebedev424@gmail.com>
2024-10-21 12:27:00 +0300

    feat: сбелал компонент UserInfo

---
+++ src/components/InfoItem/InfoItem.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+
+import styles from "./InfoItem.module.scss";
+
+export interface InfoItemProps {
+  icon: ReactNode;
+  text?: string | null;
+  isLink?: boolean;
+}
+
+export const InfoItem = ({ icon, text, isLink }: InfoItemProps) => {
+  const currentText = text || "Not Available";
+  let currentHref = "";
+
+  if (isLink) {
+    currentHref = text && text.startsWith("http") ? text : `https://${text}`;
+  }
+  return (
+    <div className={`${styles.infoItem} ${text ? "" : `${styles.empty}`}`}>
+      {icon}
+      <div>
+        {isLink && text ? (
+          <a
+            className={styles.link}
+            href={currentHref}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {currentText}
+          </a>
+        ) : (
+          currentText
+        )}
+      </div>
+    </div>
+  );
+};
